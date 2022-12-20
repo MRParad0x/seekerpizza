179792de11139419a6a97219060905fbba6792b2
MR Paradox <lahiru.chinthana22@gmail.com>
2022-12-20 17:30:54 +0530

    some changes

---
+++ dashboard.php
@@ -0,0 +1,112 @@
+<?php
+include 'conn.php';
+
+session_start();
+
+if (!isset($_SESSION['roleType'])) {
+    header('location:login.php');
+}
+
+?>
+
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset='utf-8'>
+    <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+    <title>Dashboard</title>
+    <meta name='viewport' content='width=device-width, initial-scale=1'>
+
+    <!-- custom css file link  -->
+    <link rel='stylesheet' type='text/css' media='screen' href='css/order.css'>
+
+     <!-- favicon file link  -->
+    <link rel="icon" type="image/x-icon" href="img/favicon.ico">
+
+    <!-- font awesome cdn link  -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css">
+
+    <!-- google custom font link  -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;700;800;900&display=swap" rel="stylesheet">
+
+    <link href="https://fonts.googleapis.com/css2?family=Cairo+Play:wght@200;300;400;500;600;700;800;900&display=swap" rel="stylesheet">
+
+    <link href="https://fonts.googleapis.com/css2?family=Righteous&display=swap" rel="stylesheet">
+
+    <link href="https://fonts.googleapis.com/css2?family=Courgette&display=swap" rel="stylesheet">
+
+    <link href="https://fonts.googleapis.com/css2?family=Roboto+Slab:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet">
+
+    <!-- jquery CDN -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+
+    <!-- Print component -->
+    <script type="text/javascript" src="http://www.shieldui.com/shared/components/latest/js/shieldui-all.min.js"></script>
+
+    <!-- Export component -->
+    <script type="text/javascript" src="https://unpkg.com/xlsx@0.15.1/dist/xlsx.full.min.js"></script>
+
+</head>
+
+<body>
+
+<!-- Start Verticle Menu -->
+
+<div class="flexbox" id="blur">
+<div class="box-one">
+
+    <div class="logo">
+    <img src="img/logovertical.png" alt="">
+    </div>
+
+    <?php include 'menu.php'?>
+
+    <div class="profile-box">
+    <div class="profile-logo">
+    <img src="img/profile.png" alt="">
+    <h4>Lahiru Chinthana</h4>
+    </div>
+
+    <div class="profile-setting">
+    <i class="fa-solid fa-gear"></i>
+    <a class="menubtn" href="logout.php"><i class="fa-solid fa-right-from-bracket"></i></a>
+    </div>
+    </div>
+
+</div>
+
+<!-- End Verticle Menu -->
+
+<!-- Start Header -->
+
+<div class="box-two">
+
+    <div class="header">
+    <section class="flex">
+
+    <div class="header-container">
+        <div><h1>Dashboard</h1></div>
+        <div><i class="fa-solid fa-bell"></i></div>
+        <!-- <button id="addbtn" onclick="openPopup()"> + Create Order</button> -->
+    </div>
+
+    </section>
+    </div>
+
+<!-- End Header -->
+
+    <!-- custom js file link  -->
+    <script src='js/main.js'></script>
+    <script src='js/status.js'></script>
+    <script src='js/print.js'></script>
+    <script src='js/export.js'></script>
+    <script src='js/sort.js'></script>
+</body>
+
+</html>
+
+
